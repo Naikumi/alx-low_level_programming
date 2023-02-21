bf06a2442d982f83d94b2e6be05abb95663d2970
Naikumi <naikumimartha@gmail.com>
2023-02-21 03:32:40 -0800

    prints _putchar followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
