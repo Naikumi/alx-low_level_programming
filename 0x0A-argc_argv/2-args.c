5d5cc03ed9305a7ef3e6c7cff6a231fd73b90f94
Naikumi <naikumimartha@gmail.com>
2023-03-15 02:07:43 -0700

    prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry Point
  * @argc: arguments
  * @argv: array pointing to arguments
- * 
  * Return: 0
  */
 int main(int argc, char *argv[])
