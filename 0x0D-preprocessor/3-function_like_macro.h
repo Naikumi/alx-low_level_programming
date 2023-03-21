85ee7a5137b019d20e0b2c2e332311995d8e3bf0
Naikumi <naikumimartha@gmail.com>
2023-03-20 18:42:06 -0700

    function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#ifndef ABS_H
+#define ABS_H
+#define ABS(X) (((X) < 0) ? ((X) * -1) : ((X) * 1))
 #endif
