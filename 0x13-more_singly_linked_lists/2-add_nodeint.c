90986171e81c133763a94c3035c51d5623ec70aa
Naikumi <naikumimartha@gmail.com>
2023-04-04 11:02:19 -0700

    adds new node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,28 +9,14 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
-	{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	}
 	new_node->n = n;
-	new_node->next = NULL;
-	{
-		if (*head == NULL)
-		{
-			*head = new_node;
-		}
-		else
-		{
-			listint_t *current = *head;
+	new_node->next = *head;
+	*head = new_node;
 
-			while (current->next != NULL)
-			{
-				current = current->next;
-			}
-			current->next = new_node;
-		}
-	}
-	return (*head);
+	return (new_node);
 }
