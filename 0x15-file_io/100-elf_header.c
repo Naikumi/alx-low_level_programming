5290e59d707209cedd54caf985bb25690f3648c5
Naikumi <naikumimartha@gmail.com>
2023-04-24 15:10:20 -0700

    displays the information in ELF header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * placeholder - defines a function
+  * @void: describes argument
+  * Return: Always 0 on success
+  */
+int placeholder(void)
+{
+	return (0);
+}
