281632525b10ef6d94515e30ecbe2da72d6b4f6b
Naikumi <naikumimartha@gmail.com>
2023-04-24 14:14:08 -0700

    reads and prints a textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,36 +3,33 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * read_textfile – Writes a function that reads a text file and prints it
- * @filename: a variable pointer
+ * read_textfile - reads a text file and prints it to the POSIX standard out
+ * @filename: the file to read fro
  * @letters: the size of letters
  * Return: the actual number of letters it could read and print, 0 otherwise
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fp, num_read, num_write;
-	char *txt;
-
-	txt = malloc(letters);
-	if (txt == NULL)
-		return (0);
+	ssize_t fp, num_read, rd_stat;
+	char buffer[BUFSIZE];
 
 	if (filename == NULL)
 		return (0);
-
 	fp = open(filename, O_RDONLY);
-
 	if (fp == -1)
-	{
-		free(txt);
 		return (0);
+	num_read = 0;
+	rd_stat = 1;
+	while (letters > BUFSIZE && rd_stat != 0)
+	{
+		rd_stat = read(fp, buffer, BUFSIZE);
+		write(STDOUT_FILENO, buffer, rd_stat);
+		num_read += rd_stat;
+		letters -= BUFSIZE;
 	}
-
-	num_read = read(fp, txt, letters);
-
-	num_write = write(STDOUT_FILENO, txt, num_read);
-
+	rd_stat = read(fp, buffer, letters);
+	write(STDOUT_FILENO, buffer, rd_stat);
+	num_read += rd_stat;
 	close(fp);
-
-	return (num_write);
+	return (num_read);
 }
