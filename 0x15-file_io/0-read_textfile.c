863b27fdd77e90e8c2bf05e36454a539efdf79f0
Naikumi <naikumimartha@gmail.com>
2023-05-08 13:13:05 -0700

    reads a text file and prints it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,35 @@
-#include "main.h"
-#include <stddef.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX std output
- * @filename: the file to read from
- * @letters: the letters' size
- * Return: the actual number of letters read or 0 otherwise
+ * read_textfile - reads a text file and prints it to the POSIX standard out
+ * @filename: the file to read fro
+ * @letters: the size of letters
+ * Return: the actual number of letters it could read and print, 0 otherwise
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fp, num_letters_read, rd_stat;
+	ssize_t fp, num_read, rd_stat;
 	char buffer[BUFSIZE];
 
 	if (filename == NULL)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
-		return (-1);
+		return (0);
+	num_read = 0;
+	rd_stat = 1;
 	while (letters > BUFSIZE && rd_stat != 0)
 	{
 		rd_stat = read(fp, buffer, BUFSIZE);
 		write(STDOUT_FILENO, buffer, rd_stat);
-		num_letters_read += rd_stat;
+		num_read += rd_stat;
 		letters -= BUFSIZE;
 	}
 	rd_stat = read(fp, buffer, letters);
 	write(STDOUT_FILENO, buffer, rd_stat);
+	num_read += rd_stat;
 	close(fp);
-	return (num_letters_read);
+	return (num_read);
 }
